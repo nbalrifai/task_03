6b5a3a9184be93319a8e923317a4855eab79644e
Nawaf ALrifai <nbalrifai@gmail.com>
2019-07-21 16:51:51 +0300

    Pushing

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse 
 def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
 
-    context = {
+    context = { 
+    	"my_list": [
+
+    	## restaurant 1
+    	{
+    		"restaurant_name": "Sha6ir 3abas",
+    		"food_type" : "Kuwaiti"
+    	},
+
+    	## restraurant 2
+    	{
+    		"restaurant_name": "Gogosh",
+    		"food_type" : "Farisi"
+
+    	},
+
+    	{
+    		"restaurant_name": "Bowl",
+    		"food_type" : "contemprary healthy"
+
+    	}
+
+   	]
 
     }
     return render(request, 'list.html', context)
@@ -14,6 +36,12 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    	"my_object" : {
+
+    		
+
+
+    	}
 
     }
     return render(request, 'detail.html', context)
